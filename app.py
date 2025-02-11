9cd175565d60ac80c670d7effa5fe95ac9d00f40
FIFI1803 <filipgalach@icloud.com>
2025-02-11 11:47:22 +0000

    Update .DS_Store file

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import sqlite3
 import main
+import time
+import rich as console
 
 def student_data():
     conn = sqlite3.connect('cert_project.db')
@@ -7,10 +9,10 @@ def student_data():
     c.execute('SELECT * FROM student')
     student_list = c.fetchall()
     conn.close()
-    print('========= Student Data =========')
+    console.print('[cyan][bold]====================[/bold][/cyan] [purple][bold]Student Data[/bold][/purple] [cyan][bold]====================[/bold][/cyan]')
     for student in student_list:
-        print(f'ID: {student[0]}, First Name: {student[1]}, Last Name: {student[2]}, Age: {student[3]}')
-        print('----------------------------------------')
+        console.print(f'[green][bold]First Name:[/bold][/green] {student[1]}, [green][bold]Last Name:[/bold][/green] {student[2]}, [green][bold]Age:[/bold][/green] {student[3]} [green][bold]ID:[/bold][/green] {student[0]}')
+    console.print('[cyan][bold]======================================================[/bold][/cyan]')
 
 
 def module_data():
@@ -19,28 +21,30 @@ def module_data():
     c.execute('SELECT * FROM module')
     module_list = c.fetchall()
     conn.close()
-    print('========= Module Data =========')
+    console.print('[cyan][bold]====================[/bold][/cyan] [purple][bold]Module Data[/bold][/purple] [cyan][bold]====================[/bold][/cyan]')
     for module in module_list:
-        print(f'ID: {module[0]}, Module Name: {module[1]}')
-        print('----------------------------------------')
+        console.print(f'[green][bold]ID:[/bold][/green] {module[0]}, [green][bold]Module Name:[/bold][/green] {module[1]}')
+    console.print('[cyan][bold]=====================================================[/bold][/cyan]')
+
 
 def edit_student():
-    print('========= Edit Student =========')
-    student_id = input('Enter student ID you want to change or press 0 to quit:')
+    console.print('[cyan][bold]====================[/bold][/cyan] [purple][bold]Edit Student[/bold][/purple] [cyan][bold]====================[/bold][/cyan]')
+    student_id = input('Enter student ID or press 0 to quit:')
     if student_id == '0':
         main.navigation()
     new_first_name = input('Enter new first name:')
     new_last_name = input('Enter new last name:')
     new_age = input('Enter new age:')
-    
     conn = sqlite3.connect('cert_project.db')
     c = conn.cursor()
     c.execute('UPDATE student SET first_name = ?, last_name = ?, age = ? WHERE id = ?', (new_first_name, new_last_name, new_age, student_id))
     conn.commit()
+    conn.close()
+
 
 def edit_module():
-    print('========= Edit Module =========')
-    module_id = input('Enter module ID you want to change or press 0 to quit:')
+    console.print('[cyan][bold]====================[/bold][/cyan] [purple][bold]Edit Module[/bold][/purple] [cyan][bold]====================[/bold][/cyan]')
+    module_id = input('Enter module ID or press 0 to quit:')
     if module_id == '0':
         main.navigation()
     new_module_name = input('Enter new module name:')
@@ -49,10 +53,11 @@ def edit_module():
     c = conn.cursor()
     c.execute('UPDATE module SET module = ? WHERE id = ?', (new_module_name, module_id))
     conn.commit()
+    conn.close()
 
 def check_result():
-    print('========= Check Result =========')
-    student_id = input('Enter student ID to check result or press 0 to quit:')
+    console.print('[cyan][bold]====================[/bold][/cyan] [purple][bold]Check Result[/bold][/purple] [cyan][bold]====================[/bold][/cyan]')
+    student_id = input('Enter student ID or press 0 to quit:')
     if student_id == '0':
         main.navigation()
     conn = sqlite3.connect('cert_project.db')
@@ -60,7 +65,8 @@ def check_result():
     c.execute('SELECT student.id, result.result FROM student JOIN result on student.id = result.student_id WHERE student.id = ?', (student_id,))
     for student in c.fetchall():
         student_id, result = student
-        print(f'Student ID: {student[0]}, Result: {student[1]}')
+        console.print(f'[green][bold]Student ID:[/bold][/green] {student[0]}, [green][bold]Result:[/bold][/green] {student[1]}')
+    console.print('[cyan][bold]======================================================[/bold][/cyan]')
     conn.close()
     back = input('Press 0 to leave: ')
     if back == '0':
@@ -69,11 +75,12 @@ def check_result():
 def get_name(student_id):
     conn = sqlite3.connect('cert_project.db')
     c = conn.cursor()
-    c.execute('SELECT first_name, last_name FROM student WHERE id = ?', (student_id,))
+    c.execute('SELECT first_name, last_name, age FROM student WHERE id = ?', (student_id,))
     name = c.fetchone()
+    conn.close()
     for student in name:
-        first_name, last_name = name
-        return(f'{name[0]} {name[1]}') 
+        first_name, last_name, age = name
+        return(f'{name[0]} {name[1]} Age: {name[2]}') 
 
 def generate_report():
     grades = {
@@ -82,19 +89,20 @@ def generate_report():
         3: 'Merit',
         4: 'Distinction'
     }
-    print('========= Generate Report =========')
+    student_data()
+    console.print('[cyan][bold]====================[/bold][/cyan] [purple][bold]Generate Report[/bold][/purple] [cyan][bold]====================[/bold][/cyan]')
     unsuccessful = 0
-    student_id = input('Enter student ID to generate report or press 0 to quit: ')
+    student_id = input('Enter student ID or press 0 to quit: ')
     if student_id == '0':
         main.navigation()
     conn = sqlite3.connect('cert_project.db')
     c = conn.cursor()
     c.execute('SELECT student.id, module.module, result.result FROM student JOIN result on student.id = result.student_id JOIN module on result.module_id = module.id WHERE student.id = ?', (student_id,))
     data = c.fetchall()
-    with open(f'CertReportStudentID{student_id}.txt', 'w') as file:
+    with open(f'CertReportID{student_id}.txt', 'w') as file:
         file.write('========= Certification Report =========\n')
-        file.write(f'Student ID: {student_id} Name:{get_name(student_id)}\n')
-        file.write('----------------------------------------\n')
+        file.write(f'Student ID: {student_id} Name: {get_name(student_id)}\n')
+        file.write('========================================\n')
         for student in data:
             student_id,module, result = student
             if result < 50:
@@ -118,4 +126,7 @@ def generate_report():
         else:
             file.write(f'Overall: Partial Certification Achieved\n')
     print('Report has been generated.')
+    conn.close()
+    time.sleep(2)
     main.navigation()
+    
--- main.py
+++ main.py
@@ -1,35 +1,50 @@
 import app
 import os
+import rich as console
 
-os.system('clear')
+def clear():
+    os.system('clear')
 
 def view():
-    print('========= View Data =========')
-    print('1. Student')
-    print('2. Module')
-    view_options = input('Choose which data you want to view:')
+    clear()
+    console.print('[cyan][bold]====================[/bold][/cyan] [purple][bold]View Data[/bold][/purple] [cyan][bold]====================[/bold][/cyan]')
+    console.print('[green][bold]1.[/bold][/green] Student')
+    console.print('[green][bold]2.[/bold][/green] Module')
+    console.print('[cyan][bold]===================================================[/bold][/cyan]')
+
+    view_options = input('Enter option (1-2) or press 0 to quit:')
     if view_options == '1':
+        clear()
         app.student_data()
         if input('Press 0 to leave: ') == '0':
             navigation()
     elif view_options == '2':
+        clear()
         app.module_data()
         if input('Press 0 to leave: ') == '0':
             navigation()
+    elif view_options == '0':
+        navigation()
     else:
+        clear()
         print('Invalid input. Please try again.')
         view()
 
 def edit():
-    print('========= Edit Data =========')
-    print('1. Student')
-    print('2. Module')
-    edit_options = input('Choose which data you want to add or press 0 to quit:')
+    clear()
+    console.print('[cyan][bold]====================[/bold][/cyan] [purple][bold]Edit Data[/bold][/purple] [cyan][bold]====================[/bold][/cyan]')
+    console.print('[green][bold]1.[/bold][/green] Student')
+    console.print('[green][bold]2.[/bold][/green] Module')
+    console.print('[cyan][bold]===================================================[/bold][/cyan]')
+
+    edit_options = input('Enter option (1-2) or press 0 to quit:')
     if edit_options == '1':
+        clear()
         app.student_data()
         app.edit_student()
         navigation()
     elif edit_options == '2':
+        clear()
         app.module_data()
         app.edit_module()
         navigation()
@@ -38,30 +53,34 @@ def edit():
     else:
         print('Invalid input. Please try again.')
         edit()
+
 def result():
+    clear()
     app.check_result()
 
 def get_report():
+    clear()
     app.generate_report()
 
 
 def navigation():
-    print('========= Certification Management System =========')
-    print('1. View Data')
-    print('2. Edit Data')
-    print('3. Check Result')
-    print('4. Generate Report')
-    navigation_options = input('Choose which option you want to use or press 0 to quit:')
+    clear()
+    console.print('[cyan][bold]=========[/bold][/cyan] [purple][bold]Certification Management System[/bold][/purple] [cyan][bold]=========[/bold][/cyan]')
+    console.print('[green][bold]1.[/bold][/green] View Data')
+    console.print('[green][bold]2.[/bold][/green] Edit Data')
+    console.print('[green][bold]3.[/bold][/green] Check Result')
+    console.print('[green][bold]4.[/bold][/green] Generate Report')
+    console.print('[cyan][bold]===================================================[/bold][/cyan]')
+    navigation_options = input('Enter option (1-4) or press 0 to quit:')
     if navigation_options == '1':
         view()
     elif navigation_options == '2':
         edit()
     elif navigation_options == '3':
-        result()
+         result()
     elif navigation_options == '4':
         get_report()
     elif navigation_options == '0':
-        print('Goodbye')
         exit()
     else:
         print('Invalid input. Please try again.')
